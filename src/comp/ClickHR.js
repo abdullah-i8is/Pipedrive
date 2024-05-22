3a58162692564dd047ade1ce832d6926bdf801e2
Fatimazohra316 <120090708+Fatimazohra316@users.noreply.github.com>
2024-05-22 17:20:25 +0500

    5/22/2024

--- src/comp/ClickHR.js
+++ src/comp/ClickHR.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function ClickHR() {
+    const email = localStorage.getItem("email")
+    const password = localStorage.getItem("password")
     return (
         <div style={{ padding: '70px 0 0 65px' }}>
-            <iframe src="http://click-hr.com:8000/" style={{ width: "100%", height: "100vh", overflow: "hidden", border: "none" }} />;
+            <iframe src={`http://click-hr.com:8000/?email=${email}&password=${password}`} style={{ width: "100%", height: "100vh", overflow: "hidden", border: "none" }} />;
         </div>
     )
 }
