09ad578437bfe4f29f5ae67ab2851e5b31f6659c
Fatimazohra316 <120090708+Fatimazohra316@users.noreply.github.com>
2024-05-22 14:23:02 +0500

    5/22/2024

--- src/comp/Navbar/Header.js
+++ src/comp/Navbar/Header.js
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav className="navbar">
       {/* <div className="logo"> */}
       <img width={40} src={"https://i8is.com/wp-content/uploads/2023/09/FavIcon-new-min.png"} />
-      {/* <h1 style={{ color: "#19355F" }}>s SUIT</h1> */}
+      <h1 style={{ color: "#19355F" }}>INFINITI SUIT</h1>
       {/* </div> */}
       <div className="search-bar">
         <input style={{
