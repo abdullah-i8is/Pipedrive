ba5dfea7707d265c39cb2bb83174ba1966387970
Fatimazohra316 <120090708+Fatimazohra316@users.noreply.github.com>
2024-05-24 15:03:06 +0500

    5/24/2024

--- src/comp/Caiif.js
+++ src/comp/Caiif.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 function Caiif() {
+    const email = localStorage.getItem("email")
+    const password = localStorage.getItem("password")
     return (
         <div style={{ padding: '70px 0 0 65px' }}>
-            <iframe src={`https://caiif.ca/dashboard`} style={{ width: "100%", height: "100vh", overflow: "hidden !important", border: "none" }} />;
+            <iframe src={`https://caiif.ca/dashboard?email=${email}&password=${password}`} style={{ width: "100%", height: "100vh", overflow: "hidden !important", border: "none" }} />;
         </div>
     )
 }
